da00e955ee8a580d902b2ff107d413ca5cbc0c9e
toao <m18218982596@163.com>
2022-07-25 12:52:21 +0800

    ccy

---
+++ first_week/曹春榆/mr/rpc.go
@@ -0,0 +1,51 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import "os"
+import "strconv"
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+// Add your RPC definitions here.
+type InitArgs struct {
+}
+
+type InitReply struct {
+	NReduce int
+	NMap    int
+}
+type CallTaskArgs struct {
+	DoneId int
+}
+
+type CallTaskReply struct {
+	TaskId   int
+	Phase    JobPhase
+	FileName string
+}
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the coordinator.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func coordinatorSock() string {
+	s := "/var/tmp/824-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
